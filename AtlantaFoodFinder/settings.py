502411056c7a2f302ec03d95d72e1276b41685db
Theo <theomhalpern@gmail.com>
2024-09-04 18:53:38 -0400

    chore: switch to postgreSQL

--- AtlantaFoodFinder/settings.py
+++ AtlantaFoodFinder/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'AtlantaFoodFinder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'atlfoodfinder',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
