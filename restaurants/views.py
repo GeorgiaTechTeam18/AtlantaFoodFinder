e734cce3b55735bf35c16793d7796a02ac5f2c54
dumax315 <theomhalpern@gmail.com>
2024-09-11 16:08:08 -0400

    added default response for search with form

--- restaurants/views.py
+++ restaurants/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    searchQuery = request.GET.get('q', None);
+    page = """
+
+    <form action="/" method="get">
+      <label for="q">Restaurant Search</label><br>
+      <input type="text" id="q" name="Restaurant Search" ><br>
+      <input type="submit" value="Submit">
+    </form>        """
+    if (searchQuery == None):
+        return HttpResponse(page)
