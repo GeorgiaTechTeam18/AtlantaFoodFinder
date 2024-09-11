3e490fb6b8c57bf0548d42ec94c3d07daeb6cca6
Alex Hool <64939304+alexhool@users.noreply.github.com>
2024-09-11 16:38:52 -0400

    feat: user can search and get response from Google Places API

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Create your views here.
 def index(request):
-    searchQuery = request.GET.get('q', None);
+    searchQuery = request.GET.get('q', None)
     page = """
-
-    <form action="/" method="get">
+    <form method="get">
       <label for="q">Restaurant Search</label><br>
-      <input type="text" id="q" name="Restaurant Search" ><br>
+      <input type="text" id="q" name="q" ><br>
       <input type="submit" value="Submit">
-    </form>        """
+    </form>"""
     if (searchQuery == None):
         return HttpResponse(page)
+    searchResults = requests.post('https://places.googleapis.com/v1/places:searchText', json={
+        "textQuery" : searchQuery
+    }, headers={
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": os.getenv('GOOGLE_API_KEY'),
+        'X-Goog-FieldMask': 'places.displayName,places.formattedAddress,places.priceLevel',
+    })
+    return HttpResponse(page + "You searched for: " + searchQuery + "\nAPI: " + searchResults.text)
