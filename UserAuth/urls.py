ffc2743eb63ebf08d43a0903eeb5b8609c340dd9
Seegobin, Varindra <vseegobin3@gatech.edu>
2024-09-11 22:04:18 -0400

    Registration and login now works (without email verifications). TODO: add email verifications and logout

--- UserAuth/urls.py
+++ UserAuth/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/register/', views.register, name='register'),
-
+    path('register/', views.register, name='register'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
 ]
