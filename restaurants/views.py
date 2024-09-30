c5be605b952b2a91e39e45b2ebd2f728487ac7bc
Theo <theomhalpern@gmail.com>
2024-09-30 16:34:08 -0400

    cleaned up the cuisine type

--- restaurants/views.py
+++ restaurants/views.py
@@ -143,7 +143,7 @@ def restaurant_detail_view(request, place_id):
     context = {
         'place_id': place_id,
         'form': form,
-        'cuisineType': ", ".join(details["types"]),
+        'cuisineType': ", ".join(details["types"]).title().replace("_", " "),
         'contactInformation': details["nationalPhoneNumber"],
         'address': details["formattedAddress"],
         'rating': details["rating"],
